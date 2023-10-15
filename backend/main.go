c3475f48c51f8e95a4dd562ef9489e2913da2ec4
Satoki <43191933+satokiy@users.noreply.github.com>
2023-10-16 08:51:03 +0900

    fix(backend): 環境変数の読み込みを修正

--- backend/main.go
+++ backend/main.go
@@ -13,9 +13,11 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	s3cfg := aws.NewS3Config(
